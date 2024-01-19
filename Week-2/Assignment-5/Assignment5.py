07df0771ce0ce72a7e104fdf9438bf0c6a274398
Aaron Wang <qazxsw523625@gmail.com>
2024-01-19 21:02:12 +0800

    assignment 5, final version

--- Week-2/Assignment-5/Assignment5.py
+++ Week-2/Assignment-5/Assignment5.py
@@ -2,11 +2,12 @@ def binary_search_first(numbers, target):
     if target > numbers[-1]:
         return None
     low, high = 0, len(numbers) - 1
+    ## low will always track our target, and if low <= high means only one element left
     while low <= high:
         middle = (low + high) // 2
         guess = numbers[middle]
         if guess == target:
-            while numbers[middle] == numbers[middle - 1]:
+            while guess == numbers[middle - 1]:
                 middle -= 1
             return middle
         if guess > target:
@@ -21,7 +22,3 @@ print(binary_search_first([1, 2, 5, 5, 5, 6, 7], 2))
 print(binary_search_first([1, 2, 5, 5, 5, 6, 7], 5))
 # should print 2 (since it can’t find number 3, return the index of ‘the smallest number larger than 3', that is, the index of the ‘first’ number 5)
 print(binary_search_first([1, 2, 5, 5, 5, 6, 7], 3))
-
-print(binary_search_first([1, 2, 5, 5, 5, 6, 7 , 8 , 9 , 10 , 12 , 14 , 16], 13))
-print(binary_search_first([1, 2, 5, 5, 5, 6, 7 , 8 , 9 , 10 , 12 , 14 ], 13))
-print(binary_search_first([1, 2, 5, 5, 5, 6, 7 , 8 , 9 , 10 , 12 , 14 ], 19))
--- Week-2/Assignment-5/temp.py
+++ Week-2/Assignment-5/temp.py
@@ -25,12 +25,14 @@ def binary_search_first(numbers, target):
         return result + middle + 1
 
 
-print(binary_search_first([1, 2, 5, 5, 5, 6, 7], 2))
-# should print 2 (the index of the ‘first’ target number ‘5’ shows up)
-print(binary_search_first([1, 2, 5, 5, 5, 6, 7], 5))
-# should print 2 (since it can’t find number 3, return the index of ‘the smallest number larger than 3', that is, the index of the ‘first’ number 5)
-print(binary_search_first([1, 2, 5, 5, 5, 6, 7], 3))
+# print(binary_search_first([1, 2, 5, 5, 5, 6, 7], 2))
+# # should print 2 (the index of the ‘first’ target number ‘5’ shows up)
+# print(binary_search_first([1, 2, 5, 5, 5, 6, 7], 5))
+# # should print 2 (since it can’t find number 3, return the index of ‘the smallest number larger than 3', that is, the index of the ‘first’ number 5)
+# print(binary_search_first([1, 2, 5, 5, 5, 6, 7], 3))
 
-print(binary_search_first([1, 2, 5, 5, 5, 6, 7 , 8 , 9 , 10 , 12 , 14 ], 13))
+# print(binary_search_first([1, 2, 5, 5, 5, 6, 7 , 8 , 9 , 10 , 12 , 14 ], 13))
 
-print(binary_search_first([1, 2, 5, 5, 5, 6, 7 , 8 , 9 , 10 , 12 , 14 , 16 , 17 , 18], 15))
+# print(binary_search_first([1, 2, 5, 5, 5, 6, 7 , 8 , 9 , 10 , 12 , 14 , 16 , 17 , 18], 15))
+
+
