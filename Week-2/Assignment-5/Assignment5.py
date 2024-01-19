48819c1b63119fa6fb8af7628d7a4d16383f9cfa
Aaron Wang <qazxsw523625@gmail.com>
2024-01-19 20:24:27 +0800

    use while loop for assignment5

--- Week-2/Assignment-5/Assignment5.py
+++ Week-2/Assignment-5/Assignment5.py
@@ -1,28 +1,19 @@
 def binary_search_first(numbers, target):
-    """
-    think of base case and recursive case
-    """
-    # if target is larger than the biggest value in list, return None
     if target > numbers[-1]:
         return None
-    middle = len(numbers) // 2
-    # the list will only have one element if no match at all
-    if len(numbers) == 1:
-        return middle
-    if numbers[middle] == target:
-        # check if there is any identical value (從middle往後推)
-        while numbers[middle] == numbers[middle - 1]:
-            middle -= 1
-        return middle
-    elif numbers[middle] > target:
-        numbers = numbers[:middle]
-        return binary_search_first(numbers, target)
-    elif numbers[middle] < target:
-        numbers = numbers[middle + 1:]
-        result = binary_search_first(numbers, target)
-        # add middle is considering the len of the left part
-        # add 1 is considering the start index of the right part
-        return result + middle + 1
+    low, high = 0, len(numbers) - 1
+    while low <= high:
+        middle = (low + high) // 2
+        guess = numbers[middle]
+        if guess == target:
+            while numbers[middle] == numbers[middle - 1]:
+                middle -= 1
+            return middle
+        if guess > target:
+            high = middle - 1
+        else:
+            low = middle + 1
+    return low 
 
 # should print 1 (the index of the target number ‘2’)
 print(binary_search_first([1, 2, 5, 5, 5, 6, 7], 2))
@@ -32,4 +23,5 @@ print(binary_search_first([1, 2, 5, 5, 5, 6, 7], 5))
 print(binary_search_first([1, 2, 5, 5, 5, 6, 7], 3))
 
 print(binary_search_first([1, 2, 5, 5, 5, 6, 7 , 8 , 9 , 10 , 12 , 14 , 16], 13))
-# print(binary_search_first([1, 2, 5, 5, 5, 6, 7 , 8 , 9 , 10 , 12 , 14 ], 13))
+print(binary_search_first([1, 2, 5, 5, 5, 6, 7 , 8 , 9 , 10 , 12 , 14 ], 13))
+print(binary_search_first([1, 2, 5, 5, 5, 6, 7 , 8 , 9 , 10 , 12 , 14 ], 19))
--- Week-2/Assignment-5/temp.py
+++ Week-2/Assignment-5/temp.py
@@ -2,19 +2,27 @@ def binary_search_first(numbers, target):
     """
     think of base case and recursive case
     """
-    low, high = 0, len(numbers) - 1
-    while low <= high:
-        middle = (low + high) // 2
-        guess = numbers[middle]
-        if guess == target:
-            while numbers[middle] == numbers[middle - 1]:
-                middle -= 1
-            return middle
-        if guess > target:
-            high = middle - 1
-        else:
-            low = middle + 1
-    return middle 
+    # if target is larger than the biggest value in list, return None
+    if target > numbers[-1]:
+        return None
+    middle = len(numbers) // 2
+    # the list will only have one element if no match at all
+    if len(numbers) == 1:
+        return middle
+    if numbers[middle] == target:
+        # check if there is any identical value (從middle往後推)
+        while numbers[middle] == numbers[middle - 1]:
+            middle -= 1
+        return middle
+    elif numbers[middle] > target:
+        numbers = numbers[:middle]
+        return binary_search_first(numbers, target)
+    elif numbers[middle] < target:
+        numbers = numbers[middle + 1:]
+        result = binary_search_first(numbers, target)
+        # add middle is considering the len of the left part
+        # add 1 is considering the start index of the right part
+        return result + middle + 1
 
 
 print(binary_search_first([1, 2, 5, 5, 5, 6, 7], 2))
@@ -24,3 +32,5 @@ print(binary_search_first([1, 2, 5, 5, 5, 6, 7], 5))
 print(binary_search_first([1, 2, 5, 5, 5, 6, 7], 3))
 
 print(binary_search_first([1, 2, 5, 5, 5, 6, 7 , 8 , 9 , 10 , 12 , 14 ], 13))
+
+print(binary_search_first([1, 2, 5, 5, 5, 6, 7 , 8 , 9 , 10 , 12 , 14 , 16 , 17 , 18], 15))
