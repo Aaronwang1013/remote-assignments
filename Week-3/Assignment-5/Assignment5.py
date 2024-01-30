c317085968c2627e810d13c1498cb1d86277368d
Aaron Wang <qazxsw523625@gmail.com>
2024-01-30 19:21:58 +0800

    Week3 Assignment5, follow pep8 rule

--- Week-3/Assignment-5/Assignment5.py
+++ Week-3/Assignment-5/Assignment5.py
@@ -1,13 +1,11 @@
-def twoSum(nums , target):
-    ## use hash table
+def two_sum(nums, target):
+    # use hash table
     temp = {}
-    ##j will point to the number(value) and i will point to the index (key)
+    # j will point to the number(value) and i will point to the index (key)
     for i, j in enumerate(nums):
         if (target - nums[i]) in temp:
-            return [temp[target - j] , i]     
+            return [temp[target - j], i]
         temp[j] = i
 
 
-
-
-print(twoSum([2, 7, 11, 15], 9))
+print(two_sum([2, 7, 11, 15], 9))
